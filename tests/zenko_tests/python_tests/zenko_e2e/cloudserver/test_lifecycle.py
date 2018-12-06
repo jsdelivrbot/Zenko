481c035df47ff3d48653444c46f19153aa75712d
Taylor McKinnon <mail@tmacs.space>
2018-12-06 15:15:38 -0800

    disable other tests

--- tests/zenko_tests/python_tests/zenko_e2e/cloudserver/test_lifecycle.py
+++ tests/zenko_tests/python_tests/zenko_e2e/cloudserver/test_lifecycle.py
@@ -4,19 +4,19 @@ from ..fixtures import *
 from .. import conf
 
 
-def test_expiration(expiring_bucket, testfile, objkey):
-    util.mark_test('LIFECYCLE EXPIRATION')
-    expiry, bucket = expiring_bucket  # pylint: disable=unused-variable
-    bucket.put_object(
-        Body=testfile,
-        Key=objkey
-    )
-    wait_time = 0
-    now = datetime.utcnow()
-    if expiry > now:
-        wait_time = expiry - now
-    wait_time += conf.EXPIRY_INTERVAL
-    print('Waiting %i secs for lifecyle to run' % wait_time.total_seconds())
-    assert util.check_object(objkey, testfile, bucket)
-    time.sleep(wait_time.total_seconds())
-    assert not util.check_object(objkey, testfile, bucket)
+# def test_expiration(expiring_bucket, testfile, objkey):
+#     util.mark_test('LIFECYCLE EXPIRATION')
+#     expiry, bucket = expiring_bucket  # pylint: disable=unused-variable
+#     bucket.put_object(
+#         Body=testfile,
+#         Key=objkey
+#     )
+#     wait_time = 0
+#     now = datetime.utcnow()
+#     if expiry > now:
+#         wait_time = expiry - now
+#     wait_time += conf.EXPIRY_INTERVAL
+#     print('Waiting %i secs for lifecyle to run' % wait_time.total_seconds())
+#     assert util.check_object(objkey, testfile, bucket)
+#     time.sleep(wait_time.total_seconds())
+#     assert not util.check_object(objkey, testfile, bucket)
