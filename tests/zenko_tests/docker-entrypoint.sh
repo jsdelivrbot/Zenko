609a86952901923e3f14cfb9099fb9cd669bf952
Taylor McKinnon <mail@tmacs.space>
2018-12-07 12:01:21 -0800

    its works! or at least i can curl it...

--- tests/zenko_tests/docker-entrypoint.sh
+++ tests/zenko_tests/docker-entrypoint.sh
@@ -14,13 +14,13 @@ enter_and_run() {
 }
 
 # Setup our environment
-# python3 create_buckets.py
-# if [ "$?" -ne "0" ]; then
-#     exit 1
-# fi
+python3 create_buckets.py
+if [ "$?" -ne "0" ]; then
+    exit 1
+fi
 
 # Run the tests
-# enter_and_run python_tests "./run.sh $PYTHON_ARGS"
+enter_and_run python_tests "./run.sh $PYTHON_ARGS"
 # enter_and_run node_tests "npm_chain.sh test_crr test_api test_crr_pause_resume test_location_quota test_bucket_get_v2"
 
 curl http://devstoreaccount1.azure-mock.$HELM_NAMESPACE.svc.cluster.local
